b2af79e36ada2fc26e0c02c48626ccca2513b3ec
sarahjohanknecht <johankn1@msu.edu>
2018-03-20 13:12:20 -0400

    changes to munge_data to collect patches, num killed, and eco information in addition to data

--- analysis/munge_data.py
+++ analysis/munge_data.py
@@ -8,7 +8,6 @@ frames = []
 for res_dir in glob.glob(sys.argv[1]):
     print(res_dir)
     avg_data = pd.read_csv(res_dir+"/average.dat", delimiter= " ", skiprows=19, header=None, names = ["update","merit", "gestation", "fitness", "repro_rate", "dep_size", "copied_size", "executed_size", "dep_abundance", "prop_birth", "breed_true", "dep_depth", "generation", "neutral", "label", "true_rep_rate"], index_col=False)
-    #print(avg_data)
     task_data = pd.read_csv(res_dir+"/tasks.dat", delimiter= " ", header=None, skiprows=15, names=["update","not","nand","and","orn","or","andn","nor","xor","equ" ], index_col=False)
     resource_data = pd.read_csv(res_dir+"/resource.dat", delimiter= " ", header=None, skiprows=16, names=["update","resnot", "resand", "resor", "resnor", "resnand", "resorn","resandn","resxor","resequ", "safe"], index_col=False)
     count_data = pd.read_csv(res_dir+"/count.dat", delimiter= " ", header=None, skiprows=19, names=["update","inst_executed","popsize","genotypecount","thresholdgenotypes","numspecies","numthresholdspecies","numlineages","numbirths","numdeaths", "numbreedtrue", "numbreedtrue2", "numnobirth", "numsinglethreaded", "nummultithreaded", "nummodified"], index_col=False)
@@ -20,8 +19,19 @@ for res_dir in glob.glob(sys.argv[1]):
     phenotype_data = phenotype_data.set_index("update")
 
     all_data = pd.concat([avg_data, task_data,resource_data, count_data, phenotype_data],axis=1)
+
+    #get seed, condition (directory name), number of patches, and number killed
     all_data["seed"] = res_dir.split("/")[-2].split("_")[-1]
-    all_data["condition"] = res_dir.split("/")[-2].split("_")[-2]
+    all_data["condition"] = res_dir
+    all_data["patches"]= res_dir.split("/")[-2].split("_")[0]
+    all_data["killed"]=res_dir.split("/")[-2].split("_")[5]
+
+    #get whether ecology or not
+    if(res_dir.split("/")[-2].split("_")[-2][0:3] == "eco"):
+        all_data["ecology"] = 'Y'
+
+    else:
+        all_data["ecology"] = 'N'
 
     frames.append(all_data)
 
